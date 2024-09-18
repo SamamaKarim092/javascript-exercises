8d32ca3ffc88c0ca54ed27ce5cff09a5e2917ee6
SamamaKarim092 <samamakarim90@gmail.com>
2024-09-18 18:42:17 +0500

    First and Second Task Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+let num = Date.now() % 100;
 
+const repeatString = function(word,num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    if (word === "") {
+        return "";
+    }
+
+    let result = "";
+    for (let i = 1; i <= num; i++) {
+        result += word
+    }
+    return result;
+
+   
 };
 
 // Do not edit below this line
