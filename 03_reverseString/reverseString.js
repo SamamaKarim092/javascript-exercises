bdf1dc85189b929c6791f42ea855bcf7edb971bf
SamamaKarim092 <samamakarim90@gmail.com>
2024-09-19 14:33:12 +0500

    Exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let result = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        result += word[i]
+    }
+    return result;
+    
 };
+
+console.log(reverseString("hello there"));
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
