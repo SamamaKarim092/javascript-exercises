8c16f7078da96f469c57accbdeadf9f0f44c80e5
SamamaKarim092 <samamakarim90@gmail.com>
2024-09-20 16:00:30 +0500

    Total of 6 Exercise Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    const newArr = arr.filter(element => !num.includes(element));
+    return newArr;
 };
+
+console.log(removeFromArray([2, 5, 1, 6, 7, 9], 9, 1,2,6));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    }
+
+    if(!Number.isInteger(num1 || !Number.isInteger(num2))) {
+        return "ERROR";
+    }
+
+    if(num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        let number = 0;
+        for (let i = num2; i <= num1; i++) {
+            number = i + number;
+        }
+        return number;
+    }
+
+    else {
+        let number = 0;
+        for (let i = num1; i <= num2 ; i++) {
+            number = i + number;
+        }
+        return number;
+    }
 
 };
+
+console.log(sumAll(-14,3));
 
 // Do not edit below this line
 module.exports = sumAll;
