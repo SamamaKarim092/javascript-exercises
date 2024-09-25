7e0d5bd3a2cdd6a20dcc59d55a865cbb8d70f667
SamamaKarim092 <samamakarim90@gmail.com>
2024-09-25 16:50:02 +0500

    7 Exercise is completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (5/9) * (fahrenheit - 32);
+  let fixedCelcius = celcius.toFixed(1);
+  if (fixedCelcius === "0.0") {
+    return 0;
+  }
+  return parseFloat(fixedCelcius);
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(100))
+
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (9 / 5) * celcius + 32;
+  let fixedFahrenheit = fahrenheit.toFixed(1);
+  if (fixedFahrenheit === "0.0") {
+    return 0;
+  }
+  return parseFloat(fixedFahrenheit);
 };
+
 
 // Do not edit below this line
 module.exports = {
